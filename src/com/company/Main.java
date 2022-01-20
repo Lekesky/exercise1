36d572486d15dd562790d385d5a8cecc50c512de
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-01-20 14:23:09 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String city = "Columbus";
+    int code = 43215;
+    double temps[] = {32, 25, 27, 40, 45};
+    double avgTemps = (temps[0] + temps[1] + temps[2] + temps[3] + temps[4]) / temps.length;
+        System.out.print("City: " + city + " Zip Code: " + code + " Average High Temperature " + avgTemps);
+    }
+}
